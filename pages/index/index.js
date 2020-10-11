674bec1fa845f8af243d1b44260e507ae2eb5161
lmn741 <liu435839449>
2020-10-11 15:42:59 +0800

    修正BUG

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,26 +8,43 @@ Page({
     userInfo: {},
     userId: '',
     province: ['湖南省'],
-    provinceArray: [{id: 0,name: '湖南省'}],
+    provinceArray: [{ id: 0, name: '湖南省' }],
     provinceIndex: 0,
     city: ['长沙市'],
-    cityArray: [{id: 0,name: '长沙市'}],
+    cityArray: [{ id: 0, name: '长沙市' }],
     cityIndex: 0,
     schoolArray: [],
     school: '',
     schoolId: '',
     classArray: [],
     class: '',
-    subject: ['语文','数学','英语','生物','物理','地理','政治','历史','全科'],
+    subject: ['语文', '数学', '英语', '生物', '物理', '地理', '政治', '历史', '全科'],
     subjectIndex: 0,
     subjectId: 0,
-    subjectArray: [{id:1,name:'语文'},{id:2,name:'数学'},{id:3,name:'英语'},{id:4,name:'生物'},{id:5,name:'物理'},{id:6,name:'地理'},{id:7,name:'政治'},{id:8,name:'历史'},{id:9,name:'全科'}],
+    subjectArray: [{ id: 1, name: '语文' }, { id: 2, name: '数学' }, { id: 3, name: '英语' }, { id: 4, name: '生物' }, { id: 5, name: '物理' }, { id: 6, name: '地理' }, { id: 7, name: '政治' }, { id: 8, name: '历史' }, { id: 9, name: '全科' }],
     //角色
     role: 0,            // 0老师   1 家长
     ticketNumber: "",
   },
-  onLoad(){
-    
+  onLoad() {
+    //获取缓存内的数据，初始化数据
+    try {
+      var info = wx.getStorageSync('lastDataSource');
+      let infoObj = JSON.parse(info);
+      console.log(infoObj, 'ggggggggggggggggggggggggggg')
+      if (info) {
+        this.setData({
+          class: infoObj.class_,
+          schoolId: infoObj.schoolId,
+          school: infoObj.school,
+          // subjectId: infoObj.subject,
+          subjectIndex: infoObj.subject -1,
+          ticketNumber: infoObj.ticketNumber
+        })
+      }
+    } catch (e) {
+
+    }
   },
   onShow: function () {
     //注意，主页 onLoad可能提前于 小程序 onLaunch 执行完， 
@@ -41,41 +58,41 @@ Page({
       }
     })
   },
-  closeUl(){
-    this.setData({ 
+  closeUl() {
+    this.setData({
       'schoolArray': [],
       'classArray': []
     })
   },
   bindPickerProvince(e) {//选择省
-    this.setData({provinceIndex: e.detail.value})
+    this.setData({ provinceIndex: e.detail.value })
   },
   bindPickerCity(e) {//选择城市
-    this.setData({cityIndex: e.detail.value})
+    this.setData({ cityIndex: e.detail.value })
   },
-  bindPickerSubject(e){//选择科目
+  bindPickerSubject(e) {//选择科目
     this.setData({
       subjectIndex: e.detail.value,
       subjectId: e.detail.value
     })
   },
-  getSchoolArray(e){//获取学校列表
+  getSchoolArray(e) {//获取学校列表
     var reg = /.*[\u4e00-\u9fa5]+.*$/;
-    if(!reg.test(e.detail.value)){ 
-      wx.showToast({title: '请输入中文',icon: 'none',duration: 1500}); 
-      return; 
-    } 
+    if (!reg.test(e.detail.value)) {
+      wx.showToast({ title: '请输入中文', icon: 'none', duration: 1500 });
+      return;
+    }
     let Url = app.globalData.domain + '/auth/school/listBy';
     var that = this;
     wx.request({
       url: Url,
-      header: {'uid': app.globalData.userId},
+      header: { 'uid': app.globalData.userId },
       data: {
         schoolAlias: e.detail.value
       },
-      success:res=>{
+      success: res => {
         var resData = res.data;
-        if(resData.code == 200){
+        if (resData.code == 200) {
           that.setData({
             'schoolArray': resData.data.list
           })
@@ -83,25 +100,25 @@ Page({
       }
     })
   },
-  getCurSchool(e){  //获取当前选择得学校
+  getCurSchool(e) {  //获取当前选择得学校
     this.setData({
       school: e.currentTarget.dataset.name,
       schoolId: e.currentTarget.dataset.id,
       schoolArray: []
     });
   },
-  getClassArray(e){//获取班级列表
+  getClassArray(e) {//获取班级列表
     let Url = app.globalData.domain + '/auth/school/queryClass';
     var that = this;
     wx.request({
       url: Url,
-      header: {'uid': app.globalData.userId},
+      header: { 'uid': app.globalData.userId },
       data: {
         'schoolId': that.data.schoolId
       },
-      success:res=>{
+      success: res => {
         var resData = res.data;
-        if(resData.code == 200){
+        if (resData.code == 200) {
           that.setData({
             'classArray': resData.data.list
           })
@@ -109,46 +126,57 @@ Page({
       }
     })
   },
-  getCurClass(e){  //获取当前选择得班级
+  getCurClass(e) {  //获取当前选择得班级
     this.setData({
       class: e.currentTarget.dataset.name,
       classArray: []
     });
   },
-  analyzeInfo(){//月考分析提交
-    const { role,ticketNumber } = this.data;
-    if(role === 0){
-      if(!this.data.school || !this.data.class){
-        wx.showToast({title: '请填写完整的信息', icon: 'none', duration: 2000 });
+  analyzeInfo() {//月考分析提交
+    const { role, ticketNumber } = this.data;
+    if (role === 0) {
+      if (!this.data.school || !this.data.class) {
+        wx.showToast({ title: '请填写完整的信息', icon: 'none', duration: 2000 });
         return;
       }
-    } else{
-      if(!ticketNumber){
-        wx.showToast({title: '请填写完整的信息', icon: 'none', duration: 2000 });
+    } else {
+      if (!ticketNumber) {
+        wx.showToast({ title: '请填写完整的信息', icon: 'none', duration: 2000 });
         return;
       }
     }
+    //将选择存入本地缓存
+    let dataSource = {
+      schoolId: this.data.schoolId, class_: this.data.class,school: this.data.school,
+      subject: Number(this.data.subjectId) + 1, ticketNumber: ticketNumber
+    }
+    try {
+      wx.setStorageSync('lastDataSource', JSON.stringify(dataSource))
+    } catch (e) {
+
+    }
+    //end
     let Url = app.globalData.domain + '/auth/userRole/addUserRole';
     var that = this;
     wx.request({
       url: Url,
-      header: {'uid': app.globalData.userId},
+      header: { 'uid': app.globalData.userId },
       method: "POST",
       data: {
         weChatUserId: app.globalData.userId,
         userType: role === 0 ? 1 : 2,
         schoolId: that.data.schoolId,
-        class_: that.data.class,   
-        subject: Number(that.data.subjectId)+1,
+        class_: that.data.class,
+        subject: Number(that.data.subjectId) + 1,
         ticketNumber: ticketNumber
       },
-      success:res=>{
+      success: res => {
         var resData = res.data;
-        if(resData.code == 200 || resData.code == 103){
+        if (resData.code == 200 || resData.code == 103) {
           wx.navigateTo({
-            url: '/pages/result/result?subject='+ this.data.subject[this.data.subjectIndex] + '&role=' + role
+            url: '/pages/result/result?subject=' + this.data.subject[this.data.subjectIndex] + '&role=' + role
           });
-        } else if(resData.code === 106){
+        } else if (resData.code === 106) {
           wx.showToast({
             title: resData.msg || '准考证号不存在',
           })
@@ -157,21 +185,21 @@ Page({
     })
   },
   //切换角色
-  changeRole: function(e){
+  changeRole: function (e) {
     let role = e.currentTarget.dataset.role;
-    if(role !== null && role !== undefined){
-      this.setData({role});
+    if (role !== null && role !== undefined) {
+      this.setData({ role });
     }
   },
   //准考证
-  getAdmissionTicket: function(e){
+  getAdmissionTicket: function (e) {
     let value = e.detail.value;
-    if(value !== null && value !== undefined){
-      this.setData({ticketNumber: value});
+    if (value !== null && value !== undefined) {
+      this.setData({ ticketNumber: value });
     }
   },
   //登录接口
-  userInfoHandler: function(e){
+  userInfoHandler: function (e) {
     app.globalData.userInfo = e.detail.userInfo;
     this.setData({
       userInfo: e.detail.userInfo,
@@ -180,12 +208,12 @@ Page({
     })
     this.updateUserInfoTosServer(e.detail.userInfo)
   },
-  updateUserInfoTosServer: function(userInfo){
+  updateUserInfoTosServer: function (userInfo) {
     let Url = app.globalData.domain + '/auth/wechat/editUser';
     var that = this;
     wx.request({
       url: Url,
-      header: {'uid': app.globalData.userId},
+      header: { 'uid': app.globalData.userId },
       method: "POST",
       data: {
         openid: app.globalData.openId,
@@ -198,9 +226,9 @@ Page({
         headimgurl: userInfo.avatarUrl,
         userId: app.globalData.userId
       },
-      success:res=>{
+      success: res => {
         var resData = res.data;
-        if(resData.code == 200 || resData.code == 103){
+        if (resData.code == 200 || resData.code == 103) {
           wx.showToast({
             title: '授权成功',
           })
