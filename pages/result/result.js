5727acea4fc7cbe8a82ec03ff50ca7795381510b
Zhong Yitian <363778748@qq.com>
2020-10-14 11:33:31 +0800

    饼图提示框位置修改

--- pages/result/result.js
+++ pages/result/result.js
@@ -431,7 +431,7 @@ Page({
       color:['#516b91','#59c4e6','#edafda','#93b7e3','#a5e7f0','#cbb0e3'],
       tooltip: {
           trigger: 'item',
-          position: ['15%', '15%'],
+          position: ['15%', '0'],
           textStyle: {
             'width': '80%',
             // 'height': '300px'
